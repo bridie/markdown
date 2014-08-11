b73614114754f7dd24908db335950b2a0c18a611
Henry Smith <henry@henrysmith.org>
2014-08-11 14:04:03 +0100

    Add test for multiple paragraphs

--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -8,5 +8,9 @@ describe Converter do
       expect(converter.convert_paragraph("test")).to eq "<p>test</p>"
     end
 
+    it "wraps multiple paragraphs in <p> tags" do
+      expect(converter.convert_paragraph("test\nanother line")).to eq "<p>test</p><p>another line</p>"
+    end
+
   end
 end
